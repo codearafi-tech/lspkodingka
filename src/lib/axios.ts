c8cc74d9587ab6926841025b7db247be85e29859
Dirga <arfilaks.dev@gmail.com>
2026-09-07 11:47:11 +0700

    testing login

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // Untuk Cookie Refresh Token
+//   withCredentials: true, // Untuk Cookie Refresh Token
   headers: {
     "Content-Type": "application/json",
   },
