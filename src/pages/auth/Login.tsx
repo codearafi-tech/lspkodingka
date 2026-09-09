69a7cf8fefc1b1ee812ed60d5e0504f6179308a2
Dirga <arfilaks.dev@gmail.com>
2026-09-09 13:23:21 +0700

    add padding in login hehe

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -92,7 +92,7 @@ export default function Login() {
 
   return (
     <section className="flex min-h-svh flex-col items-center justify-center bg-linear-to-br from-sky-50 via-background to-indigo-50/40 p-6">
-      <div className="flex w-full max-w-md flex-col gap-6 md:p-10 rounded-4xl border bg-white">
+      <div className="flex w-full max-w-md flex-col gap-6 p-6 md:p-10 rounded-4xl border bg-white">
         <img
           src="/images/Kredo-Logo-Only.png"
           alt="Kredo LOGO"
